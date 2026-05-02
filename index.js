a86c40519fd36f65cf2492e54bc94a50167775b4
sssolano <ssolanocr132@gmail.com>
2026-05-02 13:32:02 -0600

    Work for showing password on both input fields

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
-const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
-"/"];
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h",
+    "i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^",
+    "&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"
+];
 
+let firstEl = document.getElementById("firstOption-el").value
+let secondEl = document.getElementById("secondOption-el").value
 
+let passwordOptionOne = []
+let passwordOptionTwo = []
 
+function randomPassword() {
+    activatePasswordGenerator = true
+    for (let i = 0; i < 15; i++) {
+        
+        let passwordCharacter = Math.floor(Math.random() * characters.length)
+        
+        // password option one
+        passwordOptionOne += characters[passwordCharacter] 
+        document.getElementById("firstOption-el").value = passwordOptionOne
 
+        // password option two
+        passwordOptionTwo += characters[passwordCharacter]
+        document.getElementById("secondOption-el").value = passwordOptionTwo
+    }
+}
